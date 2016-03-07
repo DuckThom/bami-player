ca3488ab9d0dd75a88336bb81526bb2eda7d0de1
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-03-07 10:07:32 +0100

    Add command to play playlist via commandline using mps-youtube

---
+++ app/Console/Commands/HostAudioServer.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Video;
+use Illuminate\Console\Command;
+
+class HostAudioServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start playing YouTube videos (Requires mps-youtube)';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        while (true)
+        {
+            $upcoming = Video::getUpcoming();
+
+            if (isset($upcoming[0]))
+            {
+                $this->info('Now playing: ' . $upcoming[0]->name);
+                exec('mpsyt playurl ' . $upcoming[0]->id);
+                sleep(1);
+            } else
+            {
+                $this->info('Playlist empty... waiting 5 seconds...');
+                sleep(5);
+            }
+        }
+    }
+}
