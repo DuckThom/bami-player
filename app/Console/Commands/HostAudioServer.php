c22955e7e390ba4428728fe0ab8d72ecd744e97d
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-03-07 10:19:25 +0100

    Add comments

--- app/Console/Commands/HostAudioServer.php
+++ app/Console/Commands/HostAudioServer.php
@@ -36,18 +36,33 @@ class HostAudioServer extends Command
      */
     public function handle()
     {
+        // Loop forever, use Ctrl-C to stop the command
         while (true)
         {
+            $this->info('Use Ctrl-C to stop the server');
+
+            // Get the upcoming list
             $upcoming = Video::getUpcoming();
 
             if (isset($upcoming[0]))
             {
+                // Show which video we are about to play
                 $this->info('Now playing: ' . $upcoming[0]->name);
+
+                // Execute mps-youtube to play the video from the command line
                 exec('mpsyt playurl ' . $upcoming[0]->video_id);
+
+                // Remove the video from upcoming
+                Video::archive($upcoming[0]->video_id);
+
+                // Sleep for 1 second
                 sleep(1);
             } else
             {
+                // No videos in upcoming
                 $this->info('Playlist empty... waiting 5 seconds...');
+
+                // Sleep 5 seconds
                 sleep(5);
             }
         }
